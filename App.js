b9a47448c732479c3648755f1b46c2ec1ab8cc3d
Ayush Singh <as6090194@gmail.com>
2023-11-26 09:46:03 +0530

    Initialized React-Project

--- App.js
+++ App.js
@@ -4,12 +4,12 @@ import Main from "./src/components/Main"
 const App = () => {
     return (
         <div>
-            <h1>My First React Project</h1>
+            <h1></h1>
             <Main />
         </div>
     )
 }
 
 const root = createRoot(document.getElementById("root"));
-
 root.render(<App/>)
+
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,19 @@
 const Main = () => {
     return (
+        <div className="">
+        <div className=" bg-orange-400 rounded-3xl mx-5 my-10 "> 
+        <img className=" rounded-[50%] w-auto h-10 m-9" 
+        src="https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg" 
+        alt="Food" />
+        </div>
+
         <div>
-            <h2>I am the Main Component</h2>
+
+        </div>
+
+        <div>
+
+        </div>
         </div>
     )
 }
